81f715a290fa3e18a198de730a872949a2da7a34
piyush <piyush@edulab.dk>
2018-11-24 16:52:28 +0100

    added firebase and upload image

--- src/component/ImageFrame.js
+++ src/component/ImageFrame.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './UploadImage.css';
-import { Icon, Image } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 
 export default function ImageFrame(props) {
-    const { id } = props.data;
+    const { id, url } = props.data;
     return (
         <div className="imageFrame">
-            <Image className="innerImage">
-            </Image>
+            <img
+                className="uploadedImage"
+                src={url}
+                alt={id}
+                width="100%"
+                height="100%"
+            >
+            </img>
             <div className="imageControls">
+                <Icon name="caret square left" disabled size="big" />
+                <Icon name="caret square right" disabled size="big" />
                 <Icon
                     className="closeIcon"
                     size="big"
--- src/component/UploadImage.js
+++ src/component/UploadImage.js
@@ -1,23 +1,53 @@
 import React from 'react'
-import { Button, Image } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import './UploadImage.css';
+import { storage } from '../firebase';
+
+const STATE_CHANGED = 'state_changed';
 
 const UploadImage = (props) => {
-    const uploadButtonClicked = (e) => {
-        const randNumber = Math.floor((Math.random() * 100) + 1);
-        props.uploadImageHandler({
-            id: `image-id-${randNumber}`,
-            ur: 'image-url'
-        });
+
+    const uploadButtonClicked = (name, url) => {
+        // const randNumber = Math.floor((Math.random() * 100) + 1);
+        props.uploadImageHandler({ id: name, url: url });
+    }
+
+    const selectFileUploadHandler = (event) => {
+        const image = event.target.files[0];
+        if (image) {
+            // Upload image to Firebase storage account.
+            const uploadTask = storage.ref(`images/${image.name}`).put(image);
+            uploadTask.on(STATE_CHANGED,
+                (snapshot) => {
+                    console.log(snapshot.bytesTransferred / snapshot.totalBytes);
+                }, (error) => {
+                    console.log(error.message);
+                }, () => {
+                    // get uploaded image on successfull upload
+                    storage.ref('images').child(image.name).getDownloadURL().then(url => {
+                        uploadButtonClicked(image.name, url)
+                    })
+                })
+        }
     }
 
     return (
         <div className="imageContainer">
-            <Image size="medium" />
-            <Button className="uploadButton" onClick={() => uploadButtonClicked()}>
+            <div className="imagePlaceholder centre">
+                Upload image...
+            </div>
+            <input
+                type="file"
+                style={{ display: 'none' }}
+                ref={fileInput => this.fileInput = fileInput}
+                onChange={selectFileUploadHandler}>
+            </input>
+            <Button
+                className="uploadButton"
+                onClick={() => this.fileInput.click()}>
                 Upload Image
             </Button>
-        </div>
+        </div >
     )
 }
 
